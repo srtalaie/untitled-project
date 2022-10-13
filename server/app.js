8d6eda0a7153160146fb0169c895dacda88b2b56
srtalaie <srt4721@yahoo.com>
2022-10-13 16:48:37 -0700

    linting

--- server/app.js
+++ server/app.js
@@ -1,41 +1,42 @@
-const express = require("express")
+const express = require('express')
+
 const app = express()
-require("express-async-errors")
-const cors = require("cors")
-const mongoose = require("mongoose")
+require('express-async-errors')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-const blogRouter = require("./controllers/blogController")
-const usersRouter = require("./controllers/userController")
-const loginRouter = require("./controllers/loginController")
+const blogRouter = require('./controllers/blogController')
+const usersRouter = require('./controllers/userController')
+const loginRouter = require('./controllers/loginController')
 
-const { MONGODB_URI } = require("./utils/config")
-const logger = require("./utils/logger")
-const middleware = require("./utils/middleware")
+const { MONGODB_URI } = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
-logger.info("connecting to", MONGODB_URI)
+logger.info('connecting to', MONGODB_URI)
 
 mongoose
-	.connect(MONGODB_URI)
-	.then(() => {
-		logger.info("connected to MongoDB")
-	})
-	.catch((error) => {
-		logger.error("error connecting to MongoDB:", error.message)
-	})
+  .connect(MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
-app.use(express.static("build"))
+app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
 
-app.use("/api/users", usersRouter)
-app.use("/api/blogs", middleware.userExtractor, blogRouter)
-app.use("/api/login", loginRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/blogs', middleware.userExtractor, blogRouter)
+app.use('/api/login', loginRouter)
 
-if (process.env.NODE_ENV === "test") {
-	const testingRouter = require("./controllers/testing")
-	app.use("/api/testing", testingRouter)
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
 }
 
 app.use(middleware.unknownEndpoint)
--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const app = require("./app")
-const http = require("http")
-const { PORT } = require("./utils/config")
-const logger = require("./utils/logger")
+const http = require('http')
+const app = require('./app')
+const { PORT } = require('./utils/config')
+const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
 server.listen(PORT, () => {
-	logger.info(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
