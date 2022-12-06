2cddae02beb566d04e794daadf1be0a9d45be562
srtalaie <srt4721@yahoo.com>
2022-12-06 15:27:58 -0800

    test blog is displayed

--- client/cypress/e2e/Blog/blog.cy.js
+++ client/cypress/e2e/Blog/blog.cy.js
@@ -1,19 +1,27 @@
 describe('Login E2E', () => {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
     cy.createUser({
       name: 'Test Test',
       email: 'test@test.com',
       username: Cypress.env('username'),
       password: Cypress.env('password'),
     })
+
     cy.login({
       username: Cypress.env('username'),
       password: Cypress.env('password'),
     })
+
+    cy.createBlog({
+      title: 'Test Blog',
+      author: 'Test Author',
+      url: 'www.test.com',
+    })
   })
 
-  it('blog page displayed', () => {
-    cy.contains('logged in')
+  it('intial blog is displayed', () => {
+    cy.contains('Test Blog - Test Author')
   })
 })
--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -43,3 +43,18 @@ Cypress.Commands.add('createUser', ({ name, email, username, password }) => {
   }
   cy.request('POST', 'http://localhost:3003/api/users', user)
 })
+
+Cypress.Commands.add('createBlog', ({ title, author, url }) => {
+  cy.request({
+    url: 'http://localhost:3003/api/blogs',
+    method: 'POST',
+    body: { title, author, url },
+    headers: {
+      Authorization: `bearer ${
+        JSON.parse(localStorage.getItem('loggedInUser')).token
+      }`,
+    },
+  })
+
+  cy.visit('http://localhost:3000')
+})
