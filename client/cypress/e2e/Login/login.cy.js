fa7e50a319f82a4120852ef5c7c4d27d6b8bbf09
srtalaie <srt4721@yahoo.com>
2022-12-06 14:56:24 -0800

    login test update

---
+++ client/cypress/e2e/Login/login.cy.js
@@ -0,0 +1,41 @@
+describe('Login E2E', () => {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Test Test',
+      email: 'test@test.com',
+      username: Cypress.env('username'),
+      password: Cypress.env('password'),
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('home page can be opened', function () {
+    cy.contains('Blogger')
+  })
+
+  it('login page can be reached', () => {
+    cy.contains('login').click()
+  })
+
+  it('login fails with incorrect credentials', () => {
+    cy.contains('login').click()
+
+    cy.get('input[name=Username]').type('WrongUserName')
+    cy.get('input[name=Password]').type('WrongPassWord')
+    cy.get('#login-btn').click()
+
+    cy.contains('invalid username or password')
+  })
+
+  it('user is able to login', () => {
+    cy.contains('login').click()
+
+    cy.get('input[name=Username]').type(Cypress.env('username'))
+    cy.get('input[name=Password]').type(Cypress.env('password'))
+    cy.get('#login-btn').click()
+
+    cy.contains('logged in')
+  })
+})
