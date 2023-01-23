d2eebac9cdb6cb87224a27b8758443c964e5fe38
srtalaie <srt4721@yahoo.com>
2023-01-23 15:22:05 -0800

    user form mui

--- client/src/components/UserForm.js
+++ client/src/components/UserForm.js
@@ -5,6 +5,8 @@ import { useNavigate } from 'react-router-dom'
 import { setNotification } from '../reducers/notificationReducer'
 import { createAUser } from '../reducers/userReducer'
 
+import { Button, FormControl, Grid, TextField } from '@mui/material'
+
 const UserForm = () => {
   const [name, setName] = useState('')
   const [username, setUsername] = useState('')
@@ -39,57 +41,74 @@ const UserForm = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name
-          <input
-            type="text"
-            value={name}
-            name="Name"
-            id="name"
-            placeholder="Full Name"
-            onChange={({ target }) => setName(target.value)}
-          />
-        </div>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            id="username"
-            placeholder="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          email address
-          <input
-            type="text"
-            value={email}
-            name="Email"
-            id="email"
-            placeholder="Email"
-            onChange={({ target }) => setEmail(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            id="password"
-            placeholder="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button id="submit-btn" type="submit">
-          Create
-        </button>
-      </form>
-    </div>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: '80vh' }}
+    >
+      <Grid item direction={'column'}>
+        <h2>New User</h2>
+        <form onSubmit={handleSubmit}>
+          <Grid item xs={'auto'}>
+            <FormControl margin={'normal'}>
+              <TextField
+                InputLabelProps={{ shrink: true }}
+                required
+                value={name}
+                name="Name"
+                id="name"
+                label="Full Name"
+                onChange={({ target }) => setName(target.value)}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={'auto'}>
+            <FormControl margin={'normal'}>
+              <TextField
+                InputLabelProps={{ shrink: true }}
+                required
+                value={username}
+                name="Username"
+                id="username"
+                label="Username"
+                onChange={({ target }) => setUsername(target.value)}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={'auto'}>
+            <FormControl margin={'normal'}>
+              <TextField
+                InputLabelProps={{ shrink: true }}
+                required
+                value={email}
+                name="Email"
+                id="email"
+                label="Email"
+                onChange={({ target }) => setEmail(target.value)}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={'auto'}>
+            <FormControl margin={'normal'}>
+              <TextField
+                InputLabelProps={{ shrink: true }}
+                required
+                type={'password'}
+                value={password}
+                name="Password"
+                id="password"
+                label="Password"
+                onChange={({ target }) => setPassword(target.value)}
+              />
+            </FormControl>
+          </Grid>
+          <Button variant="outlined" type="submit" id="submit-btn">Create</Button>
+        </form>
+      </Grid>
+    </Grid>
   )
 }
 
