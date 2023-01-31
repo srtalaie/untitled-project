bf963cbbce29851bc1adb943be8cad2d28231a3f
srtalaie <srt4721@yahoo.com>
2023-01-31 14:01:59 -0800

    notif

--- client/src/components/Notification.js
+++ client/src/components/Notification.js
@@ -1,12 +1,14 @@
 import { useSelector } from 'react-redux'
 
+import { Grid, Typography } from '@mui/material'
+
 const Notification = () => {
   const message = useSelector((state) => state.notification)
 
   if (message === null) return null
 
   return (
-    <div id="notification">{message}</div>
+    <Grid item id="notification"><Typography variant='body1'>{message}</Typography></Grid>
   )
 }
 
