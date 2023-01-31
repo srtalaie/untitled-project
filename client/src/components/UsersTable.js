3cf94a226136becf11fc9d0595fca13d1ccf87e0
srtalaie <srt4721@yahoo.com>
2023-01-31 13:35:42 -0800

    User table page

--- client/src/components/UsersTable.js
+++ client/src/components/UsersTable.js
@@ -1,29 +1,32 @@
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import { Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+
 const UsersTable = () => {
   const users = useSelector((state) => state.users)
 
   return (
-    <div>
-      <h2>Users</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>user</th>
-            <th>blogs created</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td><Link to={`/users/${user.id}`}>{user.name}</Link></td>
-              <td>{user.blogs.length}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <Grid container direction={'column'}>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>user</TableCell>
+              <TableCell>blogs created</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell><Link to={`/users/${user.id}`}>{user.name}</Link></TableCell>
+                <TableCell>{user.blogs.length}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Grid>
   )
 }
 
